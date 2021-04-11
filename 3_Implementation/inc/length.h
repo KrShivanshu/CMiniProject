205679ec8017db9b452ed972fadd4dc681797f91
Kumar Shivanshu <kumarshivanshu13@gmail.com>
2021-04-12 02:33:48 +0530

    Add length.h

---
+++ 3_Implementation/inc/length.h
@@ -0,0 +1,90 @@
+/**
+ * @file length.h
+ * @author KUMAR SHIVANSUH
+ * @brief LENGTH TO LENGTH CONVERSION 
+ * @version 0.1
+ * @date 2021-04-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __LENGTH_H__
+#define __LENGTH_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief converts any length unit to Meter
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double meter(long double op );
+
+/**
+ * @brief converts any unit to Kilometer
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double kilometer(long double op );
+
+/**
+ * @brief converts any unit to centimeter
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double centimeter(long double op );
+
+
+/**
+ * @brief converts any unit to Millimeter
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double millimeter(long double op );
+
+/**
+ * @brief converts any unit to Nanometer
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double nanometer(long double op );
+
+/**
+ * @brief converts any unit to Micrometer
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double micrometer(long double op );
+
+/**
+ * @brief converts any unit to Mile
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double mile(long double op );
+
+/**
+ * @brief converts any unit to Foot
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double foot(long double op );
+
+/**
+ * @brief converts any unit to Inch
+ * 
+ * @param op 
+ * @return long double 
+ */
+long double inch(long double op );
+
+#endif
